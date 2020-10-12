afda705c0ffc7e9085964251fb5423d5f3af350d
Joe Tustin <J.Tustin@gmail.com>
2020-10-12 15:17:35 -0600

    adding in new example

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  //   let strArr = str.split("");
+  //   let reverseStr = [];
+
+  //   if (str.length < 1) {
+  //     return str;
+  //   }
+
+  //   return str.split("").reverse().join("");
+
+  //   for (i = str.length - 1; i >= 0; i--) {
+  //     reverseStr.push(strArr[i]);
+  //   }
+
+  //   return reverseStr.join("");
+
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
 
 module.exports = reverse;
