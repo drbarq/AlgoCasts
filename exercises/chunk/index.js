8aced62d0a35ccab2a70a16edd6a7d1a2365c65b
Joe Tustin <J.Tustin@gmail.com>
2020-10-12 17:29:05 -0600

    Adding in new example

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -37,8 +37,8 @@ function chunk(array, size) {
   let chunked = [];
   let index = 0;
   while (index < array.length) {
-    chunked.push(array.slice(index, size));
-    index = index + size;
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
 
   return chunked;
