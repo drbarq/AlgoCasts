3c6287b6a37bb6e427a779fb77becceed263d3f7
Joe Tustin <J.Tustin@gmail.com>
2020-10-18 16:09:30 -0600

    adding in solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const words = [];
+
+  for (let word of str.split(" ")) {
+    const uppserWord = word[0].toUpperCase() + word.slice(1);
+    words.push(uppserWord);
+  }
+  return words.join(" ");
+}
+
+// function capitalize(str) {
+//   let finishedSentence = [];
+
+//   let sentenceArr = str.split(" ");
+
+//   console.log(sentenceArr);
+
+//   for (let i = 0; i <= sentenceArr.length; i++) {
+//     let upperCaseLetter = sentenceArr[i][0].toUpperCase();
+
+//     let uppercaseWord = upperCaseLetter + sentenceArr[i].slice(1);
+
+//     console.log(uppercaseWord);
+//     finishedSentence.push(uppercaseWord);
+//     // finishedSentence = finishedSentence + " " + uppercaseWord;
+//     console.log(finishedSentence);
+//   }
+
+//   console.log("hello");
+//   console.log(finishedSentence);
+
+//   return finishedSentence;
+// }
 
 module.exports = capitalize;
